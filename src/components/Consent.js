004ebc0432940b3f9a2bba882238758b92a0369c
Todd <ihatespam@gmail.com>
2019-11-06 08:01:20 -0600

    Add files via upload

---
+++ src/components/Consent.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// import { navigate } from 'gatsby-link'
+// import { FaEnvelope } from 'react-icons/fa';
+import CookieConsent from "react-cookie-consent";
+
+
+
+const Consent = () => (
+
+
+<CookieConsent
+	debug={true}
+	location="bottom"
+	style={{ background: "#222", zIndex:'0', }}
+	enableDeclineButton={false}
+	declineButtonText="No Cookies"
+    declineButtonStyle={{ color: "#fff", background: "#666", fontSize: "13px", }}
+    
+    buttonText="Accept"
+	buttonStyle={{ color: "#333", background: "#eee", fontSize: "13px", fontWeight:'bold', borderRadius:'5px', padding:'.5rem 1rem', }}
+
+    expires={30}
+    cookieName="Hytron.com-site-pref-cookie"
+>
+
+    <div style={{padding:'0 0 0 1rem', fontSize:'90%',}}> We use cookies to optimize our website.{" "}<br />
+    <span style={{ fontSize: "80%" }}>
+   <a href="/privacy/" style={{color:'#fff',}}>Do Not Sell My Personal Information</a> | <a href="/privacy/" style={{color:'#fff',}}>Privacy Policy</a>
+    </span><br /><br /></div>
+    
+</CookieConsent>
+
+
+)
+
+export default Consent
+
+
---
+++ src/pages/index.md
@@ -0,0 +1,19 @@
+---
+templateKey: index-page
+title: Precision Parts
+image: /img/Ad2Grid_Logo.jpg
+heading: Full Service
+subheading: Marketing Agency
+subsubheading: Add BS HERE
+image2: /img/pexels-photo-2182863.jpg
+i2Heading: Semiconductors
+i2Subheading: Unique solutions on demand
+i2Subsubheading: Great options available
+i2Subsubheading2: Fits in your pocket!
+image3: /img/brain-digital-illustration.jpg
+i3Heading: Medical Suppliers
+i3Subheading: Doggity
+i3Subsubheading: Doggone
+i3Subsubheading2: DogPiles
+---
+
