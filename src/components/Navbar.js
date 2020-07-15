08b689217228c0ce20524874e687cff55860786d
twilightscapes <ihatespam@gmail.com>
2020-07-15 01:20:55 -0500

    Revision 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Headroom from "react-headroom"
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import { GoArrowUp } from 'react-icons/go';
 // import PopContact from '../components/PopContact'
-// import ScrollAnimation from 'react-animate-on-scroll'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 
 const Navbar = class extends React.Component {
@@ -17,16 +17,15 @@ const Navbar = class extends React.Component {
 	    
           
 <Headroom style={{ zIndex: '5', }}>
+ 
     <header className="header">
-    
-    <Link to="/" className="logolink" title="Logo - Back To Home">
-    <img className="logo" src={logo} alt="Ad2Inc Logo" style={{position:'absolute', height:'200',}} />
-    </Link>
-    
-   
 
-   
-    
+    <ScrollAnimation animateIn="backInDown"  delay={0} initiallyVisible={false} animateOnce={true} animatePreScroll={true}  offset={0}>
+    <Link to="/" className="logolink" title="Logo - Back To Home">
+    <img className="logo" src={logo} alt="Ad2Inc Logo" style={{position:'absolute', height:'200', animation: 'bounceInDown .5s'}} />
+   </Link>
+  </ScrollAnimation>
+  
 <input className="menu-btn" type="checkbox" id="menu-btn" />
 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
 
@@ -99,6 +98,7 @@ const Navbar = class extends React.Component {
       
       
       <a href="#topofpage" title="Back To Top" className="back-to-top "><GoArrowUp /></a>
+      
 </Headroom>
         
       
