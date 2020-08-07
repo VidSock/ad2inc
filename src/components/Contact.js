a6a3b354574dca76035bd4e16885e6a80b5c6831
twilightscapes <ihatespam@gmail.com>
2020-08-07 03:24:15 -0500

    LH fixes cont

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -89,54 +89,54 @@ export default class Contact extends React.Component {
 
                 <div className="form">
                 <div className="field half left">
-                <label title="Your Name" htmlFor="name" style={{padding: '0', margin: '0',}}>Your name:</label>
+                <label title="Your Name" htmlFor="name1" style={{padding: '0', margin: '0',}}>Your name:</label>
                     <input
                       className="input"
                       type={'text'}
                       name={'name'}
                       onChange={this.handleChange}
-                      id={'name'}
+                      id={'name1'}
                       required={true}
                       placeholder="Name"
                       title="Your Name"
                     />
-                <label title="Your Email" htmlFor="email" style={{padding: '0', margin: '0',}}>Your email:</label>
+                <label title="Your Email" htmlFor="email1" style={{padding: '0', margin: '0',}}>Your email:</label>
                     <input
                       className="input"
                       type={'email'}
                       name={'email'}
                       onChange={this.handleChange}
-                      id={'email'}
+                      id={'email1'}
                       required={true}
                       placeholder="your@email.com"
                       title="Your Email"
                     />
                     <label title="Upload File" htmlFor="attachment" style={{padding: '0', margin: '0',}}>Upload file:</label>
                     <div className="upload" style={{ border: '0px solid', padding: '12px', borderRadius: '5px', boxShadow: '0px 0px 1px 1px #999', color: '#999',}}>
-                    <label title="Zip preferred" htmlFor="attachment" style={{padding: '0', margin: '0 2.5% 0 0', float: 'left', width: '58%', color: '#333',}}>
+                    <label title="Zip preferred" htmlFor="attachment1" style={{padding: '0', margin: '0 2.5% 0 0', float: 'left', width: '58%', color: '#333',}}>
                     ZIP files preferred
                     <input
                         className="file-input hidden"
                         type="file"
-                        id="attachment"
+                        id="attachment1"
                         name="attachment"
                         onChange={this.handleAttachment}
                         title="file attachment"
                       />
                       </label>
                       
-<label title="upload" className="upload" htmlFor="attachment" style={{background: '#eee', width: '48%', justifyContent: 'center', color: '#333', padding: '5px 15px', textAlign: 'center', borderRadius:'4px', margin: '0 auto', fontSize: '70%', whiteSpace: 'nowrap',}}  >Select File</label></div>
+<label title="upload" className="upload" htmlFor="attachment1" style={{background: '#eee', width: '48%', justifyContent: 'center', color: '#333', padding: '5px 15px', textAlign: 'center', borderRadius:'4px', margin: '0 auto', fontSize: '70%', whiteSpace: 'nowrap',}}  >Select File</label></div>
                 </div>
 
                 <div className="field half right">
-<label title="Your Message" className="label" htmlFor={'message'} style={{padding: '0', margin: '0',}}>
+<label title="Your Message" className="label" htmlFor={'message1'} style={{padding: '0', margin: '0',}}>
                     Your message
                   </label>
                     <textarea
                       className="textarea"
                       name={'message'}
                       onChange={this.handleChange}
-                      id={'message'}
+                      id={'message1'}
                       required={true}
                       placeholder="Message"
                       title="Type Your Message Here"
@@ -147,11 +147,11 @@ export default class Contact extends React.Component {
                   </div>
                   </div>
                 
-                <div style={{fontSize: '70%', padding: '0 3%', margin: '0 0 1rem 0', textAlign: 'center',}}> <label title="Agree" htmlFor={'consent'}><input title="Agree to terms" type="checkbox"  name={'consent'} value={'given'} required={true} style={{transform: 'scale(2)', margin: '0 10px 0 0', padding: '0',}} />  I want to communicate by email or text.</label></div>
+                <div style={{fontSize: '70%', padding: '0 3%', margin: '0 0 1rem 0', textAlign: 'center',}}> <label title="Agree" htmlFor={'consent'}><input title="Agree to terms" id="consent" type="checkbox"  name={'consent'} value={'given'} required={true} style={{transform: 'scale(2)', margin: '0 10px 0 0', padding: '0',}} />  I want to communicate by email or text.</label></div>
                
 <div className="special grad"  style={{color:'#fff', textDecoration:'none', display:'flex', justifyContent:'center', position:'relative', width:'230px', margin:'0 auto', padding:'0 1rem 0px 1rem',}}>
                 
-                <input title="Submit" type="submit" value="Send Message" className=" grad" style={{background:'transparent', border:'none', color:'#fff', cursor:'pointer', width:'90%', float:'left',}} />
+                <input id="submit" title="Submit" type="submit" value="Send Message" className=" grad" style={{background:'transparent', border:'none', color:'#fff', cursor:'pointer', width:'90%', float:'left',}} />
                 <FaEnvelope style={{position: 'relative', top: '5px', right: '-5px', color: '#fff', fontSize: '120%',}} />
                 
                 </div>
